ed7d8e734f3c2d8e711bc07ed78c007011c6afc6
Diyorbek <85473674+DiyorbekUz@users.noreply.github.com>
2022-09-03 22:56:50 +0500

    Create index.js
    Finish

---
+++ index.js
@@ -0,0 +1,34 @@
+
+// 1-solution
+function hide_card(card){
+    let newCardNumber = ''
+    card = card.replace(/\s/g, '');
+
+    for(let i = 0; i<= card.length-1; i++){
+        if(i <= 3){
+            newCardNumber+=card[i]
+            continue
+        }
+        if (i > 11) {
+            newCardNumber+=card[i]
+            continue
+        }
+
+        newCardNumber+='*'
+
+    }
+
+    newCardNumber = newCardNumber.match(/.{1,4}/g).join(' ')
+    return newCardNumber
+}
+
+console.log(hide_card('4276 4500 0000 1313'))
+
+// 2-solution
+
+
+function percentage(num, percent){
+    return num * (percent / 100);
+}
+
+console.log(percentage(100, 0.03))
